84ea69f09d8a83aef55af9459dd938bf87691e6f
MacroFPGA <56591865+MacroFPGA@users.noreply.github.com>
2019-10-18 11:40:46 +0100

    Initial Check-in

---
+++ releases/build_rom_Devil Fish.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set zip=devilfsg.zip
+set ifiles=dfish1.7f+dfish2.7h+dfish3.7k+dfish4.7m+dfish5.1h+dfish6.1k+82S123.6e
+set ofile=a.devilfsh.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
